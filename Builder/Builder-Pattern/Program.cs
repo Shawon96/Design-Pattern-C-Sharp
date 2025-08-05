a64f2bd5dd47701f9a02550c0e01a802e458a007
shawon963 <moniruzzaman.akand@technonext.com>
2025-08-05 21:37:05 +0600

    update for gitignore and without builder

---
+++ Builder/Builder-Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+var hello = "hello";
+
+var sb = new StringBuilder();
+sb.Append("<p>");
+sb.Append(hello);
+sb.Append("</p>");
+
+
+
+var words = new[] { "hello", "world" };
+sb.Clear();
+sb.Append("<ul>");
+foreach (var word in words)
+{
+    sb.AppendFormat("<li>" + word + "</li>");
+}
+sb.Append("</ul>");
+Console.WriteLine(sb);
